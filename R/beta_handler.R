bb23300554e92c8da1dfac98d85043c1dc16f7a0
vaslem <vaslemonidis@hotmail.com>
2026-02-11 16:44:39 +0100

    Enhance BetaHandler to return beta matrix rows based on numeric row names

--- R/beta_handler.R
+++ R/beta_handler.R
@@ -428,6 +428,9 @@ BetaHandler <- R6::R6Class("BetaHandler", # nolint
             if (!is.null(private$.bsseq_object)) {
                 .log_step("Extracting beta values from BSseq object..", level = 3)
                 beta_mat <- getMeth(private$.bsseq_object, type = "raw")
+                if (all(is.numeric(row_names))) {
+                    return (beta_mat[row_names, , drop = FALSE])
+                }
                 all_row_names <- self$getBetaRowNames()
                 rownames(beta_mat) <- all_row_names
                 if (!is.null(row_names)) {
