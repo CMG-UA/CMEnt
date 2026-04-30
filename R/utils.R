ca13a460aa6a5f091d8f19ab75ba163a5c8272a9
vaslem <vaslemonidis@hotmail.com>
2026-04-30 22:34:42 +0200

    Fix loadExampleInputData to use load instead of readRDS for loading local resources; update make_data script permissions

--- R/utils.R
+++ R/utils.R
@@ -2863,7 +2863,7 @@ loadExampleInputData <- function(resource, use_experiment_hub = TRUE) {
         if (verbose_setting >= 2) {
             .log_info("Loading ", resource, " from existing local path", level = 2)
         }
-        return(readRDS(local_path))
+        return(load(local_path))
     }
 
     # Fall back to ExperimentHub when local example files are unavailable.
@@ -2912,7 +2912,7 @@ loadExampleInputData <- function(resource, use_experiment_hub = TRUE) {
         if (verbose_setting >= 2) {
             .log_info("Loading ", resource, " from existing local path", level = 2)
         }
-        return(readRDS(local_path))
+        return(load(local_path))
     }
 
     # If we get here, the resource was not found
